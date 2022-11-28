452f48d8620336a1f44026b4074a8ed6e18c9aa7
tienthienngt1 <tienthienngt2@gmail.com>
2022-11-28 22:50:34 +0700

    fix mess again

--- components/commons/component/MessFb.tsx
+++ components/commons/component/MessFb.tsx
@@ -1,15 +1,36 @@
-//@ts-ignore
-import MessengerCustomerChat from "react-messenger-customer-chat";
+import { useEffect } from "react";
 
 const MessFb = () => {
+	useEffect(() => {
+		var chatbox = document.getElementById("fb-customer-chat");
+		chatbox && chatbox.setAttribute("page_id", "111485028459651");
+		chatbox && chatbox.setAttribute("attribution", "biz_inbox");
+		//@ts-ignore
+		window.fbAsyncInit = function () {
+			//@ts-ignore
+			FB.init({
+				xfbml: true,
+				version: "v15.0",
+			});
+		};
+
+		(function (d, s, id) {
+			var js,
+				fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s);
+			js.id = id;
+			//@ts-ignore
+			js.src =
+				"https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
+			//@ts-ignore
+			fjs.parentNode.insertBefore(js, fjs);
+		})(document, "script", "facebook-jssdk");
+	}, []);
 	return (
 		<>
-			<MessengerCustomerChat
-				pageId="100088136385934"
-				appId="488516726488262"
-				xfbml={true}
-			/>
-			,
+			<div id="fb-root"></div>
+			<div id="fb-customer-chat" className="fb-customerchat"></div>
 		</>
 	);
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,21 @@ import type { AppProps } from "next/app";
 import { ThemeConfig, GlobalStyle } from "config";
 import { store } from "redux/store";
 import { Provider } from "react-redux";
+import ToTopButton from "components/commons/component/ToTopButton";
+import MessFb from "components/commons/component/MessFb";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Provider store={store}>
-			<ThemeConfig>
-				<Component {...pageProps} />
-				<GlobalStyle />
-			</ThemeConfig>
-		</Provider>
+		<>
+			<Provider store={store}>
+				<ThemeConfig>
+					<Component {...pageProps} />
+					<GlobalStyle />
+				</ThemeConfig>
+			</Provider>
+			<ToTopButton />
+			<MessFb />
+		</>
 	);
 }
 
